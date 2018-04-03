adbb13be4263590a865134e7bd6771e04152e3eb
rodgerljl@msn.com <rodgerljl@msn.com>
2018-04-02 21:52:59 -0700

    more comments

--- 0_classifier/image_classifier.py
+++ 0_classifier/image_classifier.py
@@ -35,15 +35,15 @@ classes = ('plane', 'car', 'bird', 'cat',
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 6, 5) # 3-channel images, 6 outputs, 5x5 kernel 
+        self.pool = nn.MaxPool2d(2, 2) # pooling layer is to reduce the size of the representations
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120) # fc stands for "fully connected"
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x))) # ReLU (Rectified Linear Units) increases the nonlinear properties
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
