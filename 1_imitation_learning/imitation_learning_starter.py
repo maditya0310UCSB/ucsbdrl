811f135bd8930f2374ea0ece99c939d8600140fa
rodgerljl@msn.com <rodgerljl@msn.com>
2018-04-09 13:23:42 -0700

    add comments

--- 1_imitation_learning/imitation_learning_starter.py
+++ 1_imitation_learning/imitation_learning_starter.py
@@ -1,6 +1,22 @@
+#==========================================================================  
+# Imitation Learning Starter Code 
+# 
+# The code was initially written for UCSB Deep Reinforcement Learning Seminar 2018
+#
+# Authors: Jieliang (Rodger) Luo, Sam Green
+#
+# April 9th, 2018
+#==========================================================================
+
 import gym
 import getch
 
+num_episodes = 5
+
+# build neural network here 
+
+# keyboard controls
+# a -> left, s -> don't move, d -> right
 def humanInput():
 
 	char = getch.getch()
@@ -14,32 +30,45 @@ def humanInput():
 
 	return a
 
-env = gym.make('MountainCar-v0')
+def main():
 
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
+    # create environment
+    env = gym.make('MountainCar-v0')
 
-num_episodes = 5
+    print(env.action_space) # 3 actions: push left, no push, push right
+    print(env.observation_space) # 2 observations: position, velocity 
+    print(env.observation_space.high) # max position & velocity: 0.6, 0.07
+    print(env.observation_space.low) # min position & velocity: -1.2, -0.07
 
-for episode in range(num_episodes):
-    
-    observation = env.reset()
-    done = False
-    step = 0
-    actions = []
-    observations = [] 
+    # initialize network, loss function, etc. here
 
-    while not done:
-        env.render()
-        #print(observation)
-        action = humanInput()
-        observation, reward, done, info = env.step(action)
-
-        observations.append(observation)
-        actions.append(action)
-        step += 1
+    # learning from expert (user inputs)
+    for episode in range(num_episodes):
         
-    print("Episode finished after {} steps".format(step))
+        observation = env.reset()
+        done = False
+        step = 0
+        actions = []
+        observations = [] 
+
+        while not done:
+            env.render()
+            print(observation)
             
+            action = humanInput()
+            observation, reward, done, info = env.step(action)
+
+            # store all the observations and actions from one episode
+            observations.append(observation)
+            actions.append(action)
+            
+            step += 1
+            
+        print("Episode finished after {} steps".format(step))
+
+        # train the network here 
+
+    # evaluate the network when the training is finished 
+            
+if __name__ == "__main__":
+    main()
